c046b2d4c96c409619ca40a3ce83f56b022cc3c1
Gamer4life <lpuvilla0001@mymail.lausd.net>
2019-10-10 16:36:37 +0000

    On part 3, section Password Reset View

--- myproject/boards/tests/test_views.py
+++ myproject/boards/tests/test_views.py
@@ -1,11 +1,10 @@
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import home, board_topics
 from django.contrib.auth.models import User
-from .views import new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from ..views import home, board_topics, new_topic
+from ..models import Board, Topic, Post
+from ..forms import NewTopicForm
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'widget_tweaks',
 
+    'accounts',
+
     'boards',
 ]
 
@@ -128,3 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
