b9a263996e4141393944a7228f3866c3d4918058
Gamer4life <lpuvilla0001@mymail.lausd.net>
2019-11-07 16:30:20 +0000

    Fixed second pipeline error

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -128,9 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
